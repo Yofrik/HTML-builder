b5813d51d4e0e7c050dde5169e3a247d4e959756
Vladislav Tikhonovich <vt@rmz.by>
2023-05-08 10:46:57 +0300

    feat: completed tasks 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('node:path');
+const process = require('node:process');
+const directPath = path.join(process.cwd(), '03-files-in-folder', 'secret-folder');
+
+function readToDirect(directPath) {
+    fs.readdir(directPath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.error('Ошибка чтения директории:', err);
+        } 
+        else {
+            files.forEach((file) => {
+            if (file.isFile()) {
+                const filePath = path.join(directPath, file.name);
+                const fileInfo = path.parse(filePath);
+                fs.stat(filePath, (err, stats) =>{
+                    if (err) {
+                        console.error('Ошибка чтения параметров файла:', err);
+                      } 
+                    else{
+                        console.log(`${path.parse(filePath).name} - ${path.extname(file.name)} - ${stats.size} bytes`)
+                    };
+                }
+            )};
+            });
+        }
+      });
+}
+
+readToDirect(directPath);
